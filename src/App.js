980c44d897c7cb69e1e82a191e55e0407047b528
Shahid <shahid.sarker@gmail.com>
2020-06-20 23:19:56 -0400

    react-redux changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import "./index.css";
+import noteReducer from "./reducers/noteReducer";
 
-function App() {
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "the app state is in redux store",
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "state changes are made with actions",
+    important: false,
+    id: 2,
+  },
+});
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+
+const App = () => {
+  const addNote = (e) => {
+    e.preventDefault();
+    const content = e.target.note.value;
+    e.target.note.value = "";
+    store.dispatch(createNote(content));
+  };
+
+  const createNote = (content) => {
+    return {
+      type: "NEW_NOTE",
+      data: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    };
+  };
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: "TOGGLE_IMPORTANCE",
+      data: { id },
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={addNote}>
+        <input type="text" name="note" />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
+            {note.content} <strong>{note.important ? "important" : ""}</strong>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+// export default App;
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
+
+export default renderApp();
+store.subscribe(renderApp);
