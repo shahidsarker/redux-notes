4be8db8efe48cd0b881adc1fc6365613d8c8ada3
Shahid <shahid.sarker@gmail.com>
2020-06-20 23:56:00 -0400

    Extract reducer and implement react-redux

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
 import "./index.css";
-import noteReducer from "./reducers/noteReducer";
-
-const store = createStore(noteReducer);
-
-store.dispatch({
-  type: "NEW_NOTE",
-  data: {
-    content: "the app state is in redux store",
-    important: true,
-    id: 1,
-  },
-});
-
-store.dispatch({
-  type: "NEW_NOTE",
-  data: {
-    content: "state changes are made with actions",
-    important: false,
-    id: 2,
-  },
-});
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+import { createNote, toggleImportanceOf } from "./reducers/noteReducer";
+import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const notes = useSelector((state) => state);
+
   const addNote = (e) => {
     e.preventDefault();
     const content = e.target.note.value;
     e.target.note.value = "";
-    store.dispatch(createNote(content));
-  };
-
-  const createNote = (content) => {
-    return {
-      type: "NEW_NOTE",
-      data: {
-        content,
-        important: false,
-        id: generateId(),
-      },
-    };
+    dispatch(createNote(content));
   };
 
   const toggleImportance = (id) => {
-    store.dispatch({
-      type: "TOGGLE_IMPORTANCE",
-      data: { id },
-    });
+    dispatch(toggleImportanceOf(id));
   };
 
   return (
@@ -59,7 +25,7 @@ const App = () => {
         <button type="submit">add</button>
       </form>
       <ul>
-        {store.getState().map((note) => (
+        {notes.map((note) => (
           <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? "important" : ""}</strong>
           </li>
@@ -69,11 +35,4 @@ const App = () => {
   );
 };
 
-// export default App;
-
-const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById("root"));
-};
-
-export default renderApp();
-store.subscribe(renderApp);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,79 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
 import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
 import noteReducer from "./reducers/noteReducer";
 
 const store = createStore(noteReducer);
-
-store.dispatch({
-  type: "NEW_NOTE",
-  data: {
-    content: "the app state is in redux store",
-    important: true,
-    id: 1,
-  },
-});
-
-store.dispatch({
-  type: "NEW_NOTE",
-  data: {
-    content: "state changes are made with actions",
-    important: false,
-    id: 2,
-  },
-});
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
-
-const App = () => {
-  const addNote = (e) => {
-    e.preventDefault();
-    const content = e.target.note.value;
-    e.target.note.value = "";
-    store.dispatch(createNote(content));
-  };
-
-  const createNote = (content) => {
-    return {
-      type: "NEW_NOTE",
-      data: {
-        content,
-        important: false,
-        id: generateId(),
-      },
-    };
-  };
-
-  const toggleImportance = (id) => {
-    store.dispatch({
-      type: "TOGGLE_IMPORTANCE",
-      data: { id },
-    });
-  };
-
-  return (
-    <div>
-      <form onSubmit={addNote}>
-        <input type="text" name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {store.getState().map((note) => (
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? "important" : ""}</strong>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-// export default App;
-
-const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById("root"));
-};
-
-export default renderApp();
-store.subscribe(renderApp);
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
+);
