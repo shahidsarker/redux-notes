20378c8b463c8737b659c2d04882668ece6c41e7
Shahid <shahid.sarker@gmail.com>
2020-06-26 01:07:57 -0400

    notes service and initialize notes

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import App from "./App";
 import { createStore, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-import noteReducer from "./reducers/noteReducer";
+import noteReducer, { initializeNotes } from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
+
+import noteService from "./services/notes";
 
 const reducer = combineReducers({
   notes: noteReducer,
@@ -15,6 +17,10 @@ const reducer = combineReducers({
 });
 
 const store = createStore(reducer, composeWithDevTools());
+
+noteService.getAll().then((notes) => {
+  store.dispatch(initializeNotes(notes));
+});
 
 ReactDOM.render(
   <Provider store={store}>
